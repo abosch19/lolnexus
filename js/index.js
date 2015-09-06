20cb2338c65a3813809b4d1dc7b8e8950574e3ce
Alex <abosch19@gmail.com>
2015-09-06 13:52:49 +0200

    Reestructurando el codigo

--- js/index.js
+++ js/index.js
@@ -17,29 +17,52 @@ $(button).on("click", searchSummoner);
 
 
 var summoner = $("[data-name]");
-var summonerName;
 
 
 function searchSummoner (event) {
   event.preventDefault();
   var c = document.getElementById('region');
   var region = c.value;
-  summonerName = $(username).val().toLowerCase().replace(/ /g,'');
+  console.log("OK");
+  var summonerName = $(username).val().toLowerCase().replace(/ /g,'');
   var summonerPath = "https://" + region + API_URL_ROOT + region + API_URL_SUMMONERNAME + summonerName + "/?api_key=" + API_KEY;
   $.getJSON(summonerPath, getID).fail(errorFound);
 }
 
-//Errro si no encuentra summoner
+
+//Error si no encuentra summoner
 function errorFound (jqxhr, textStatus, err) {
-  var error = textStatus + ", " + error;
-  alert("Request Failed: " + err);
+  if(!$(username).val("")){
+    var error = textStatus + ", " + error;
+    alert("Request Failed: " + err);
+  }
 }
 
 //Rellena los campos del summoner
 function getID (data) {
+  if(!$(username).val() == ""){
+    var c = document.getElementById('region');
+    var region = c.value;
+    var summonerName = $(username).val().toLowerCase().replace(/ /g,'');
+    var id = document.getElementById('id');
+    var lvl = document.getElementById('lvl');
 
-  var c = document.getElementById('region');
-  var region = c.value;
+    id.innerHTML = "Name: " + data[summonerName].name;
+    lvl.innerHTML = "Lvl: " + data[summonerName].summonerLevel;
+    getIcon(summonerName, data, region);
+    getRankedStats(summonerName, data, region);
+
+
+    console.log(data);
+    $(username).val("");
+  }
+}
+
+
+
+
+function getIcon(summonerName, data, region) {
+  var icon = document.getElementById('img');
   var iconPath = API_ICON_URL_DDRAGON + data[summonerName].profileIconId + ".png";
 
  //Cambia el path de la imagen/icono si no funciona con ddragon
@@ -50,15 +73,14 @@ function getID (data) {
     iconPath = API_ICON_URL_AVATAR + region + "/" + summonerName + ".png";
     icon.src = iconPath;
   })
+}
 
-  var id = document.getElementById('id');
-  var lvl = document.getElementById('lvl');
-  var icon = document.getElementById('img');
+
+
+
+function getRankedStats(summonerName, dataStats, region) {
   var wins = document.getElementById('wins/losses');
-  id.innerHTML = data[summonerName].name;
-  lvl.innerHTML = data[summonerName].summonerLevel;
-
-  var winsPath = "https://"+ region + API_URL_ROOT + region + API_URL_WINS + data[summonerName].id + "/summary?season=SEASON2015&api_key=" + API_KEY;
+  var winsPath = "https://"+ region + API_URL_ROOT + region + API_URL_WINS + dataStats[summonerName].id + "/summary?season=SEASON2015&api_key=" + API_KEY;
   $.getJSON(winsPath, function (data){
     console.log(data);
 
@@ -66,16 +88,13 @@ function getID (data) {
     var matched = false;
     var i;
     for(i = 0;i < 12 && !matched; ++i) {
-      console.log(data.playerStatSummaries[i].playerStatSummaryType);
       if(data.playerStatSummaries[i].playerStatSummaryType == "RankedSolo5x5") {
         console.log("Matched!");
         matched = true;
       }
     }
-    wins.innerHTML = data.playerStatSummaries[i-1].wins + "/" + data.playerStatSummaries[i-1].losses;
+    wins.innerHTML = "Ranked W/L: " + data.playerStatSummaries[i-1].wins + "/" + data.playerStatSummaries[i-1].losses;
   });
-  console.log(data);
-  $(username).val("");
 }
 
 })()
