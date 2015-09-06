e4cb84aa22a57831b81d83dd15a23e7e7f27f8e2
Alex <abosch19@gmail.com>
2015-09-06 11:12:39 +0200

    Rankeds wins/losses

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 (function() {
 
 var API_KEY = "93d33598-7d65-444e-8daa-788818f097a1";
-var API_URL1 = ".api.pvp.net/api/lol/"
-var API_URL2 ="/v1.4/summoner/by-name/";
+var API_URL_ROOT = ".api.pvp.net/api/lol/"
+var API_URL_SUMMONERNAME ="/v1.4/summoner/by-name/";
+var API_URL_WINS = "/v1.3/stats/by-summoner/"
+
 var API_ICON_URL_AVATAR = "http://avatar.leagueoflegends.com/";
 var API_ICON_URL_DDRAGON = "http://ddragon.leagueoflegends.com/cdn/5.2.1/img/profileicon/";
 
@@ -15,14 +17,16 @@ $(button).on("click", searchSummoner);
 
 
 var summoner = $("[data-name]");
+var summonerName;
 
 
 function searchSummoner (event) {
   event.preventDefault();
   var c = document.getElementById('region');
   var region = c.value;
-  var summonerName = $(username).val();
-  $.getJSON("https://" + region + API_URL1 + region + API_URL2 + summonerName + "/?api_key=" + API_KEY, getID).fail(errorFound);
+  summonerName = $(username).val().toLowerCase().replace(/ /g,'');
+  var summonerPath = "https://" + region + API_URL_ROOT + region + API_URL_SUMMONERNAME + summonerName + "/?api_key=" + API_KEY;
+  $.getJSON(summonerPath, getID).fail(errorFound);
 }
 
 //Errro si no encuentra summoner
@@ -36,7 +40,6 @@ function getID (data) {
 
   var c = document.getElementById('region');
   var region = c.value;
-  var summonerName = $(username).val()
   var iconPath = API_ICON_URL_DDRAGON + data[summonerName].profileIconId + ".png";
 
  //Cambia el path de la imagen/icono si no funciona con ddragon
@@ -48,12 +51,29 @@ function getID (data) {
     icon.src = iconPath;
   })
 
-  var id = document.getElementById('name');
+  var id = document.getElementById('id');
   var lvl = document.getElementById('lvl');
   var icon = document.getElementById('img');
-  id.innerHTML = data[summonerName].id;
+  var wins = document.getElementById('wins/losses');
+  id.innerHTML = data[summonerName].name;
   lvl.innerHTML = data[summonerName].summonerLevel;
 
+  var winsPath = "https://"+ region + API_URL_ROOT + region + API_URL_WINS + data[summonerName].id + "/summary?season=SEASON2015&api_key=" + API_KEY;
+  $.getJSON(winsPath, function (data){
+    console.log(data);
+
+    //Busca en data(json) el número de rankeds wins/losses
+    var matched = false;
+    var i;
+    for(i = 0;i < 12 && !matched; ++i) {
+      console.log(data.playerStatSummaries[i].playerStatSummaryType);
+      if(data.playerStatSummaries[i].playerStatSummaryType == "RankedSolo5x5") {
+        console.log("Matched!");
+        matched = true;
+      }
+    }
+    wins.innerHTML = data.playerStatSummaries[i-1].wins + "/" + data.playerStatSummaries[i-1].losses;
+  });
   console.log(data);
   $(username).val("");
 }
